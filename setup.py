37f106662456033d6f5edc4156a565d3a2d621be
martin.liivak <martin.liivak@gmail.com>
2019-11-30 12:38:04 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RCSnail-Commons",
+    version="1.0.0",
+    author="Martin Liivak",
+    author_email="martin.liivak@gmail.com",
+    description="RCSnail AI project's common methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "pyzmq",
+        "ruamel.yaml"
+    ]
+)
