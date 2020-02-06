eaa06becb62c2049f0e4a302cc676abfdb697113
martin.liivak <martin.liivak@gmail.com>
2020-02-06 23:40:21 +0200

    Add update mode tag.

--- commons/car_controls.py
+++ commons/car_controls.py
@@ -1,26 +1,36 @@
 
 class CarControls:
-    __slots__ = ['gear', 'steering', 'throttle', 'braking']
+    __slots__ = ['gear', 'steering', 'throttle', 'braking', 'update_mode']
 
-    def __init__(self, gear, steering, throttle, braking):
+    def __init__(self, gear, steering, throttle, braking, update_mode):
         self.gear = gear
         self.steering = steering
         self.throttle = throttle
         self.braking = braking
 
+        if update_mode not in ['supervisor', 'steer']:
+            raise ValueError
+        self.update_mode = update_mode
+
     def to_list(self):
         return [self.gear, self.steering, self.throttle, self.braking]
 
+    def to_dict(self):
+        return {element: getattr(self, element, 0) for element in self.__slots__}
+
 
 class CarControlUpdates:
-    __slots__ = ['d_gear', 'd_steering', 'd_throttle', 'd_braking', 'supervisor']
+    __slots__ = ['d_gear', 'd_steering', 'd_throttle', 'd_braking', 'update_mode']
 
-    def __init__(self, gear, steering, throttle, braking, supervisor: bool = False):
+    def __init__(self, gear, steering, throttle, braking, update_mode):
         self.d_gear = gear
         self.d_steering = steering
         self.d_throttle = throttle
         self.d_braking = braking
-        self.supervisor = supervisor
+
+        if update_mode not in ['supervisor', 'steer']:
+            raise ValueError
+        self.update_mode = update_mode
 
     def to_list(self):
         return [self.d_gear, self.d_steering, self.d_throttle, self.d_braking]
