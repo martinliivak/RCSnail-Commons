bfaf1e291c41716a2ad9f2073a991670e0481c6a
martin.liivak <martin.liivak@gmail.com>
2020-03-04 21:37:08 +0200

    Remove update mode tag.

--- commons/car_controls.py
+++ commons/car_controls.py
@@ -1,16 +1,12 @@
 
 class CarControls:
-    __slots__ = ['gear', 'steering', 'throttle', 'braking', 'update_mode']
+    __slots__ = ['gear', 'steering', 'throttle', 'braking']
 
-    def __init__(self, gear, steering, throttle, braking, update_mode):
+    def __init__(self, gear, steering, throttle, braking):
         self.gear = gear
         self.steering = steering
         self.throttle = throttle
         self.braking = braking
-
-        if update_mode not in ['supervisor', 'steer', 'steer_diff']:
-            raise ValueError
-        self.update_mode = update_mode
 
     def to_list(self):
         return [self.gear, self.steering, self.throttle, self.braking]
@@ -20,17 +16,13 @@ class CarControls:
 
 
 class CarControlUpdates:
-    __slots__ = ['d_gear', 'd_steering', 'd_throttle', 'd_braking', 'update_mode']
+    __slots__ = ['d_gear', 'd_steering', 'd_throttle', 'd_braking']
 
-    def __init__(self, gear, steering, throttle, braking, update_mode):
+    def __init__(self, gear, steering, throttle, braking):
         self.d_gear = gear
         self.d_steering = steering
         self.d_throttle = throttle
         self.d_braking = braking
-
-        if update_mode not in ['supervisor', 'steer', 'steer_diff']:
-            raise ValueError
-        self.update_mode = update_mode
 
     def to_list(self):
         return [self.d_gear, self.d_steering, self.d_throttle, self.d_braking]
