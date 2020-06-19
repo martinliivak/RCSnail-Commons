dfeee80fcdb9242fd8a3f50362b765bd7235cb94
martin.liivak <martin.liivak@gmail.com>
2020-06-19 14:20:26 +0300

    Add manual override flag

--- commons/car_controls.py
+++ commons/car_controls.py
@@ -16,16 +16,17 @@ class CarControls:
 
 
 class CarControlUpdates:
-    __slots__ = ['d_gear', 'd_steering', 'd_throttle', 'd_braking']
+    __slots__ = ['d_gear', 'd_steering', 'd_throttle', 'd_braking', 'manual_override']
 
-    def __init__(self, gear, steering, throttle, braking):
+    def __init__(self, gear, steering, throttle, braking, manual_override):
         self.d_gear = gear
         self.d_steering = steering
         self.d_throttle = throttle
         self.d_braking = braking
+        self.manual_override = manual_override
 
     def to_list(self):
-        return [self.d_gear, self.d_steering, self.d_throttle, self.d_braking]
+        return [self.d_gear, self.d_steering, self.d_throttle, self.d_braking, self.manual_override]
 
     def to_dict(self):
         return {element: getattr(self, element, 0) for element in self.__slots__}
